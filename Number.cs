ec5f2a979a98ff42d5eb64833deba4965ebe47cc
ArcKontyR <kontyrtime@mail.ru>
2021-01-27 11:51:38 +0500

    rw

---
+++ Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcForTest1
+{
+    class Number
+    {
+        public Number(int num)
+        {
+            this.num = num;
+        }
+        public int num {get; set;}
+
+        public bool CheckForTwoDigits()
+        {
+            if (num >= 10 && num <= 99)
+                return true;
+            else return false;
+        }
+    }
+}
